4b00f0579c6101b72baf9289c180c5adaafd42a5
EssamEldenMohamed <essam315>
2023-05-25 04:22:04 +0300

    first collection view finished

--- SportsAllTheTime/leagueDetails/leagueDetailsView/CollectionView1Cell.swift
+++ SportsAllTheTime/leagueDetails/leagueDetailsView/CollectionView1Cell.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class CollectionView1Cell: UICollectionViewCell {
     
+    @IBOutlet weak var awayTeamLable: UILabel!
+    @IBOutlet weak var homeTeamLabel: UILabel!
+    @IBOutlet weak var secondTeamImg: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var homeTeamImg: UIImageView!
 }
--- SportsAllTheTime/leagueDetails/leagueDetailsView/LeaguesDetailsViewController.swift
+++ SportsAllTheTime/leagueDetails/leagueDetailsView/LeaguesDetailsViewController.swift
@@ -8,34 +8,104 @@
 import UIKit
 
 class LeaguesDetailsViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UITableViewDelegate,UITableViewDataSource {
-
+    @IBOutlet weak var secondColletionView: UICollectionView!
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var firstCollectionView: UICollectionView!
+    var listImgs = [String]()
+    var listNames = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let layout = UICollectionViewCompositionalLayout { [self] sectionIndex, enviroment in
+                            switch sectionIndex {
+                            case 0 :
+                                return foodBannerSection()
+                            default:
+                                return foodBannerSection()
+                            }
+                        }
+                firstCollectionView.setCollectionViewLayout(layout, animated: true)
+        listImgs = ["football.jpeg","giraf.jpeg","bunny.jpeg","mario.jpeg"]
+        listNames = ["Real Madrid","Bercelona","Manchester United","Elnaser"]
     }
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        if(collectionView == firstCollectionView){
+            return listNames.count
+        }
+        return listImgs.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        if(collectionView == firstCollectionView){
+            let cell = firstCollectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath)as! CollectionView1Cell
+            cell.homeTeamImg.image = UIImage(named: listImgs[indexPath.row])
+            cell.homeTeamLabel.text = listNames[indexPath.row]
+            cell.timeLabel.text = "22:00"
+            cell.dateLabel.text = "25/3/2023"
+            return cell
+        }
+        let cell = secondColletionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath)as! CollectionView2Cell
+        cell.backgroundColor = UIColor.blue
+        return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        <#code#>
+        if(collectionView == firstCollectionView){
+            print("first")
+        }else{
+            print("second")
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)as! DetailsTableViewCell
+        cell.backgroundColor = UIColor.yellow
+        return cell
     }
-    
-
-
-  
-
+      
+     func foodBannerSection()-> NSCollectionLayoutSection {
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension:.fractionalWidth(1), heightDimension: .absolute(170))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//            group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0
+//             , bottom: 0, trailing: 15)
+            let section = NSCollectionLayoutSection(group: group)
+//            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 15
+//             , bottom: 10, trailing: 0)
+        section.visibleItemsInvalidationHandler = { (items, offset, environment) in
+             items.forEach { item in
+             let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
+             let minScale: CGFloat = 0.8
+             let maxScale: CGFloat = 1.0
+             let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
+             item.transform = CGAffineTransform(scaleX: scale, y: scale)
+             }
+        }
+            section.orthogonalScrollingBehavior = .continuous
+            return section
+        }
+   static func foodCategorySection()-> NSCollectionLayoutSection {
+           let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1)
+           , heightDimension: .fractionalHeight(1))
+           let item = NSCollectionLayoutItem(layoutSize: itemSize)
+           
+       let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.75)
+                                              , heightDimension: .fractionalHeight(0.10))
+           let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize
+           , subitems: [item])
+           group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0
+           , bottom: 0, trailing: 15)
+           
+           let section = NSCollectionLayoutSection(group: group)
+           section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 15
+           , bottom: 10, trailing: 0)
+           section.orthogonalScrollingBehavior = .continuous
+           
+           return section
+       }
 }
