dd894aadc9421d594ab60b7e18ef87719810096a
EssamEldenMohamed <essam315>
2023-06-05 15:11:15 +0300

    set lottie animation

--- SportsAllTheTime/splashScreen/ViewController.swift
+++ SportsAllTheTime/splashScreen/ViewController.swift
@@ -14,10 +14,18 @@ class ViewController: UIViewController {
        
     }
     override func viewDidAppear(_ animated: Bool) {
-        let home = self.storyboard?.instantiateViewController(identifier: "home") as! UITabBarController
-        home.modalPresentationStyle = .fullScreen
-        home.modalTransitionStyle = .crossDissolve
-        self.present(home, animated: true)
+        let animationView = LottieAnimationView(name: "loadballs")
+        animationView.frame = CGRect(x: -40, y:view.bounds.height/3 , width: 500, height: 500)
+        view.addSubview(animationView)
+
+        animationView.play(fromProgress: 0, toProgress: 1, loopMode: .playOnce) { (finished) in
+            let home = self.storyboard?.instantiateViewController(identifier: "home") as! UITabBarController
+            home.modalPresentationStyle = .fullScreen
+            home.modalTransitionStyle = .crossDissolve
+            self.present(home, animated: true)
+        }
+//        animationView.animationSpeed = Float(animationView.animation!.duration) / Float(duration)
+       
        
     }
 
